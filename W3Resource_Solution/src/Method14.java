5a3bcb2c0735c09f07e14ab9e85fdfade8f667ed
Chungjung Lee <cj.chungjung.lee@gmail.com>
2017-10-04 10:32:36 +0900

    Method 14, 15 풀이&Calendar와 Formatter클래스 사용법

---
+++ W3Resource_Solution/src/Method14.java
@@ -0,0 +1,42 @@
+/*
+ * Write a Java method to create the area of a pentagon.
+ * 
+ * Expected Output:
+ * Input the number of sides: 5
+ * Input the side: 6
+ * The area of the pentagon is 61.93718642120281
+ * 
+ * */
+import java.util.Scanner;
+
+public class Method14 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Input the number of sides: ");
+		int numOfSide = sc.nextInt();
+		System.out.print("Input the side: ");
+		int lenOfSide = sc.nextInt();
+		
+		System.out.println("The area of the pentagon is " + areaPentagon(numOfSide, lenOfSide));
+		
+		sc.close();
+	}
+	
+	public static double areaPentagon(int numOfSide, int lenOfSide) {		
+		/*
+		 * n-각형의 내각의 합 = (n-2) * 180
+		 * 정오각형의 한 내각 = 108도
+		 * 삼각함수를 이용하여 삼각형의 높이를 구한다
+		 * 
+		 * */
+		double angle = (numOfSide - 2) * 180 / numOfSide;
+		double height = (lenOfSide / 2) * Math.tan(Math.toRadians(angle / 2));
+		
+		double area = (lenOfSide * height / 2) * numOfSide;
+		
+		return area;
+	}
+
+}
---
+++ W3Resource_Solution/src/Method15.java
@@ -0,0 +1,51 @@
+/*
+ * Write a Java method to display the current date and time.
+ * 
+ * Expected Output:
+ * Current date and time: Wednesday January 25, 2017 7:47:43
+ * 
+ * */
+import java.util.Calendar;
+import java.util.Formatter;
+import java.util.Locale;
+
+public class Method15 {
+
+	public static void main(String[] args) {
+		
+		timeDisplayByCalendar();
+	}
+
+	public static void timeDisplayByCalendar() {
+		/*
+		 * Formatter 클래스의 사용법
+		 * 
+		 * 포맷을 지정한 문자열이 출력될 곳은 Appendable 인터페이스를
+		 * 구현한 클래스여야 한다.
+		 * StringBuffer, PrintStream, FileWriter 등이 있다.
+		 * 
+		 * */
+		StringBuffer sb = new StringBuffer();
+		Formatter f = new Formatter(sb);
+		Calendar c = Calendar.getInstance();
+		
+		// Wednesday January 25, 2017 7:47:43
+		f.format(Locale.US, "%tA %tB %te, %tY %tk:%tM:%tS", 
+				c, c, c, c, c, c, c);
+		System.out.println(f);
+		
+		// 버퍼에 저장되어 있는 문자열들이 모두 출력이 되면서 
+		// 버퍼를 비우는 함수
+		//f.flush();
+
+		// Calendar.getInstance()에 parameter를 locale, timezone 
+		// 두 가지 보낼 수 있다.
+		// locale은 사용하는 컴퓨터의 지역을 나타내는것 같고
+		// timezone은 받아오는 시간을 지정하는것 같다.
+		Calendar cUS = Calendar.getInstance(Locale.US);
+		f.format("%tA %tB %te, %tY %tk:%tM:%tS", 
+				cUS, cUS, cUS, cUS, cUS, cUS, cUS);
+		
+		System.out.println(f);
+	}
+}
